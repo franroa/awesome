f2296cfe18270faf6fc1d76aa4595212ff333ec4
franroa <franciscoroaprieto@gmail.com>
2023-05-13 18:16:34 +0200

    [None]: FIrst try
    Why:
    
    How:
    
    Tags:
    
    Author:
    Francisco Roa Prieto
    Branch:
    main
    Jira Ticket:
    None

---
+++ autostart.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function run {
+	if ! pgrep $1; then
+		$@ &
+	fi
+}
+
+#run "megasync"
+run "xscreensaver -no-splash"
+#run "/usr/bin/dropbox"
+#run "insync start"
+run "compton"
+#run "/usr/bin/redshift"
+# run "urxvtd -q -f -o"
+run "mpd"
+run "nm-applet"
+pkill conky
+
+run "google-chrome --start-fullscreen"
+run "firefox"
+run "gnome-todo"
+run "opera --start-fullscreen"
+
+sleep 3
+run "$HOME/Scripts/Theming/1440.sh"
+#run "$HOME/Scripts/start_HUD.sh"
+#run "conky -c $HOME/.config/awesome/conky/conkyrc"
+run "conky -c $HOME/.config/awesome/conky/conkyrc.lua"
+sleep 1
+run "conky -c $HOME/.config/awesome/conky/seamod_rings.lua"
