9b1e4ded05b67d98dd31be5260b5a12fc7db9ed8
Francisco Roa Prieto <francisco.roa.prieto@synaos.com>
2023-07-24 08:41:23 +0200

    [${COMMIT_TITLE}]: changes
    Why:
    
    How:
    
    Tags:
    
    Author:
    ${AUTHOR}
    Branch:
    ${BRANCH_NAME}
    Jira Ticket:
    ${JIRA_TICKET}

--- autostart.sh
+++ autostart.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
-function run {
-	if ! pgrep $1; then
-		$@ &
-	fi
-}
-
-#run "megasync"
-run "xscreensaver -no-splash"
-#run "/usr/bin/dropbox"
-#run "insync start"
-run "compton"
-#run "/usr/bin/redshift"
-# run "urxvtd -q -f -o"
-run "mpd"
-run "nm-applet"
-pkill conky
-
-run "google-chrome --start-fullscreen"
-run "firefox"
-run "obsidian"
-run "gnome-todo"
-run "opera --start-fullscreen"
-
-sleep 3
-run "$HOME/Scripts/Theming/1440.sh"
-#run "$HOME/Scripts/start_HUD.sh"
-#run "conky -c $HOME/.config/awesome/conky/conkyrc"
-run "conky -c $HOME/.config/awesome/conky/conkyrc.lua"
-sleep 1
-run "conky -c $HOME/.config/awesome/conky/seamod_rings.lua"
+# function run {
+# 	if ! pgrep $1; then
+# 		$@ &
+# 	fi
+# }
+#
+# #run "megasync"
+# run "xscreensaver -no-splash"
+# #run "/usr/bin/dropbox"
+# #run "insync start"
+# run "compton"
+# #run "/usr/bin/redshift"
+# # run "urxvtd -q -f -o"
+# run "mpd"
+# run "nm-applet"
+# pkill conky
+#
+# run "google-chrome --start-fullscreen"
+# run "firefox"
+# run "obsidian"
+# run "gnome-todo"
+# run "opera --start-fullscreen"
+#
+# sleep 3
+# run "$HOME/Scripts/Theming/1440.sh"
+# #run "$HOME/Scripts/start_HUD.sh"
+# #run "conky -c $HOME/.config/awesome/conky/conkyrc"
+# run "conky -c $HOME/.config/awesome/conky/conkyrc.lua"
+# sleep 1
+# run "conky -c $HOME/.config/awesome/conky/seamod_rings.lua"
